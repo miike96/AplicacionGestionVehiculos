ca16d44d7e359f0422e99c65a4a9ccdb82884a1c
Miguel <92276707+miike96@users.noreply.github.com>
2024-03-05 14:17:45 +0100

    Creación del proyecto final

---
+++ src/main/java/org/cliente/gestionvehiculos/GestionVehiculosApplication.java
@@ -0,0 +1,13 @@
+package org.cliente.gestionvehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVehiculosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionVehiculosApplication.class, args);
+    }
+
+}
